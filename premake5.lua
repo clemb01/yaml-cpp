c145b1e88c57865dfb79d0fed7a034c38ffc07a6
clemb01 <32745478+clemb01@users.noreply.github.com>
2021-01-06 21:16:04 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+project "yaml-cpp"
+    kind "StaticLib"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+        "src/**.cpp"
+	}
+    
+    includedirs
+    {
+        "include"
+    }
+    
+	filter "system:linux"
+		systemversion "latest"
+
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        
+    filter "system:linux"
+        pic "On"
+		systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
